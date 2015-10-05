d7bb80f61c6c9e2b7b31ce9b3c9cdd9a71ab0033
innbtg <ainur-nafikov@rambler.ru>
2015-10-05 23:24:24 +0300

    1

--- _bla/_bla/Program.cs
+++ _bla/_bla/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace _bla
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
         }
     }
 }
